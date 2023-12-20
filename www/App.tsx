ce9faef962b0474a301c270e9303783bda334950
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2023-12-20 15:52:14 -0500

    chore: run formatter

--- www/App.tsx
+++ www/App.tsx
@@ -1,34 +1,38 @@
 import React, { useEffect, useState } from "https://esm.sh/react@18.2.0";
 import { dialog } from "https://esm.sh/@tauri-apps/api@1.5.2";
-import { tw } from 'https://cdn.skypack.dev/twind@0.16.19'
+import { tw } from "https://cdn.skypack.dev/twind@0.16.19";
 
-export default function App(){
+export default function App() {
+  const [state, setState] = useState("Click the button!");
 
-    const [state, setState] = useState("Click the button!")
+  useEffect(() => {
+    /** https://esbuild.github.io/api/#live-reload */
+    const liveReload = new EventSource("/esbuild");
+    liveReload.addEventListener("change", () => location.reload());
+    return () => {
+      liveReload.close();
+    };
+  }, []);
 
-    useEffect(() => {
-        /** https://esbuild.github.io/api/#live-reload */
-        const liveReload = new EventSource('/esbuild')
-        liveReload.addEventListener('change', () => location.reload())
-        return () => {
-            liveReload.close()
-        };
-      }, []);
+  function askUser() {
+    dialog.ask("Are you ok? :)", "Hey!").then((res: boolean) => {
+      if (res) {
+        setState("You are good!");
+      } else {
+        setState("You are not good D:");
+      }
+    });
+  }
 
-    function askUser(){
-        dialog.ask("Are you ok? :)", "Hey!").then((res: boolean) => {
-            if(res){
-                setState("You are good!")
-            } else {
-                setState("You are not good D:")
-            }
-        })
-    }
-
-    return (
-        <div className={tw`mx-10 my-5 `}>
-            <h1 className={tw`text-xl`}>{state}</h1>
-            <button className={tw`my-2 px-3 py-2 rounded-lg bg-gray-900 text-white transition duration-150 ease-in-out`} onClick={askUser}>Click me :)</button>
-        </div>
-    )
+  return (
+    <div className={tw`mx-10 my-5 `}>
+      <h1 className={tw`text-xl`}>{state}</h1>
+      <button
+        className={tw`my-2 px-3 py-2 rounded-lg bg-gray-900 text-white transition duration-150 ease-in-out`}
+        onClick={askUser}
+      >
+        Click me :)
+      </button>
+    </div>
+  );
 }
--- www/index.tsx
+++ www/index.tsx
@@ -1,6 +1,6 @@
-import React from "https://esm.sh/react@18.2.0" 
-import { createRoot } from "https://esm.sh/react-dom@18.2.0/client" 
-import App from  "./App.tsx";
+import React from "https://esm.sh/react@18.2.0";
+import { createRoot } from "https://esm.sh/react-dom@18.2.0/client";
+import App from "./App.tsx";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
