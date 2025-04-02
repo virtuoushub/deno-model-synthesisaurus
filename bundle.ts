bb026b0022c3ea843287550c5125fc8ba1dce1bc
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2025-04-02 08:14:16 -0400

    chore(deno@tauri): cool...

--- bundle.ts
+++ bundle.ts
@@ -1,4 +1,4 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.20.2/mod.js";
+import * as esbuild from "https://deno.land/x/esbuild@v0.23.1/mod.js";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
 
 const ctx = await esbuild.context({
