cf0884540c684cfdc249618fcaa283a28d44be33
Pete <petercolapietro+github@gmail.com>
2026-05-09 23:35:30 +0000

    ...

--- build.ts
+++ build.ts
@@ -1,8 +1,11 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.23.1/mod.js";
+// import * as esbuild from "https://deno.land/x/esbuild@v0.28.0/mod.js";
+import * as esbuild from "https://deno.land/x/esbuild@v0.23.0/mod.js";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
 
+const importMapURL = new URL("./import_map.json", import.meta.url).href;
+
 await esbuild.build({
-    plugins: [...denoPlugins()],
+    plugins: [...denoPlugins({ importMapURL })],
     entryPoints: ["./www/index.tsx"],
     outfile: "./www/dist/main.js",
     bundle: true,
--- bundle.ts
+++ bundle.ts
@@ -1,8 +1,10 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.23.1/mod.js";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
 
+const importMapURL = new URL("./import_map.json", import.meta.url).href;
+
 const ctx = await esbuild.context({
-  plugins: [...denoPlugins()],
+  plugins: [...denoPlugins({ importMapURL })],
   entryPoints: ["./www/index.tsx"],
   outfile: "./www/dist/main.js",
   bundle: true,
