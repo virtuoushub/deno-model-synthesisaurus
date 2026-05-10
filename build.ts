8b8301c8a2e2d4be86f8b1e9266ff1e39083604e
Pete <petercolapietro+github@gmail.com>
2026-05-10 00:38:34 +0000

    ...

--- build.ts
+++ build.ts
@@ -1,5 +1,4 @@
-// import * as esbuild from "https://deno.land/x/esbuild@v0.28.0/mod.js";
-import * as esbuild from "https://deno.land/x/esbuild@v0.23.0/mod.js";
+import * as esbuild from "https://deno.land/x/esbuild@v0.28.0/mod.js";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
 
 const importMapURL = new URL("./import_map.json", import.meta.url).href;
